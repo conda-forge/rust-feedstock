369b76558c0411c951b56de12f444d6220af6dcf
Peter Williams <peter@newton.cx>
2018-10-16 17:31:37 -0400

    recipe/build.sh: monkey with environment to get Rust to use the right linker
    On GCC7, we need to set a magic environmen variable in order to get Rust to
    use our $CC as its linker. Otherwise it just tried to run `cc`, which we don't
    provide.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,21 @@
 #!/bin/bash -e
 
 ./install.sh --prefix=$PREFIX
+
+# Fun times -- by default, Rust/Cargo tries to link executables on Linux by
+# invoking `cc`. This is not (necessarily) available on Linux/GCC7. By setting
+# a magic environment variable, we can override this default.
+
+set -ex
+
+if [ "$c_compiler" = gcc ] ; then
+    mkdir -p $PREFIX/etc/conda/activate.d $PREFIX/etc/conda/deactivate.d
+
+    cat <<'EOF' >$PREFIX/etc/conda/activate.d/rust.sh
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=$CC
+EOF
+
+    cat <<'EOF' >$PREFIX/etc/conda/deactivate.d/rust.sh
+unset CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER
+EOF
+fi

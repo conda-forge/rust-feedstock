97a215bd8aaaba8b7b8c1def493d16788dc3efcc
Ray Donnelly <mingw.android@gmail.com>
2019-05-05 18:56:41 +0100

    Missed some

---
+++ recipe/install-msvc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+pushd msvc
+
+PATH="${PWD}:$PATH" ./install.sh --prefix=${PREFIX}
+
+if [[ ${target_platform} == osx-64 ]]; then
+  if [[ $(uname) == Darwin ]]; then
+    ${INSTALL_NAME_TOOL:-install_name_tool} -add_rpath @loader_path/../../.. ${PREFIX}/lib/rustlib/x86_64-apple-darwin/lib/python2.7/site-packages/lldb/lldb-argdumper
+  else
+    echo "ERROR :: To build ${PKG_NAME} for ${target_platform} you need install_name_tool"
+    exit 1
+  fi
+fi

7815640fdaeeafab0125514ec119ddcfc3b22f50
Ray Donnelly <mingw.android@gmail.com>
2019-10-16 01:07:52 +0100

    Update to 1.38.0 (WIP)

--- recipe/install-unix.sh
+++ recipe/install-unix.sh
@@ -1,12 +1,4 @@
 #!/bin/bash -e
 
-PATH="${PWD}:$PATH" ./install.sh --prefix=${PREFIX}
-
-if [[ ${target_platform} == osx-64 ]]; then
-  if [[ $(uname) == Darwin ]]; then
-    ${INSTALL_NAME_TOOL:-install_name_tool} -add_rpath @loader_path/../../.. ${PREFIX}/lib/rustlib/x86_64-apple-darwin/lib/python2.7/site-packages/lldb/lldb-argdumper
-  else
-    echo "ERROR :: To build ${PKG_NAME} for ${target_platform} you need install_name_tool"
-    exit 1
-  fi
-fi
+[[ ${target_platform} == osx-64 ]] && cp "${RECIPE_DIR}"/xcode-select .
+PATH="${PWD}:$PATH" ./install.sh --prefix="${PREFIX}"

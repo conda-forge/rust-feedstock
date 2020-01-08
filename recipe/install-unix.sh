b2d2f0b67d9401ba198a0af34a219b6189696489
Jonathan Helmus <jjhelmus@gmail.com>
2020-01-08 09:50:39 -0600

    kludge for missing library in rustlib dir

--- recipe/install-unix.sh
+++ recipe/install-unix.sh
@@ -2,3 +2,12 @@
 
 [[ ${target_platform} == osx-64 ]] && cp "${RECIPE_DIR}"/xcode-select .
 PATH="${PWD}:$PATH" ./install.sh --prefix="${PREFIX}"
+
+# these files looks to be missing from the rustlib dir
+# TODO investigate if these are really needed
+if [[ ${target_platform} == linux-64 ]]; then
+    ln -s ${PREFIX}/lib/librustc_driver-*.so ${PREFIX}/lib/rustlib/x86_64-unknown-linux-gnu/lib/
+fi
+if [[ ${target_platform} == osx-64 ]]; then
+    ln -s ${PREFIX}/lib/librustc_driver-*.dylib ${PREFIX}/lib/rustlib/x86_64-apple-darwin/lib/
+fi

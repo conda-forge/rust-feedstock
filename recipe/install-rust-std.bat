cb3b2d126906469c04b8280fa52d00243c966c49
Isuru Fernando <isuruf@gmail.com>
2020-09-19 15:05:38 +0000

    Use better name for std

--- recipe/install-rust-std.bat
+++ recipe/install-rust-std.bat
@@ -1,3 +1,3 @@
 FOR /F "delims=" %%i in ('cygpath.exe -u "%PREFIX%/Library"') DO set "pfx=%%i"
-bash %RECIPE_DIR%/install-rust-bat.sh
+bash %RECIPE_DIR%/install-rust-std.sh
 if errorlevel 1 exit 1
--- recipe/install-rust-std.sh
+++ recipe/install-rust-std.sh
@@ -9,17 +9,6 @@ DESTDIR=$PWD/destdir/
 # to install the files corresponding to rust-std
 ./install.sh --prefix=$PREFIX --destdir=$DESTDIR
 
-case "$target_platform" in
-    linux-64) rust_arch=x86_64-unknown-linux-gnu ;;
-    linux-aarch64) rust_arch=aarch64-unknown-linux-gnu ;;
-    linux-ppc64le) rust_arch=powerpc64le-unknown-linux-gnu ;;
-    win-64) rust_arch=x86_64-pc-windows-msvc ;;
-    osx-64) rust_arch=x86_64-apple-darwin ;;
-    osx-arm64) rust_arch=aarch64-apple-darwin ;;
-    *) echo "unknown target_platform $target_platform" ; exit 1 ;;
-esac
-
-
 while read line; do
   file=$(echo $line | cut -b 6-)
   destination=$(echo $file | cut -b ${#DESTDIR}-)

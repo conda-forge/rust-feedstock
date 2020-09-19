6796f7c3ded8ad012ed6c7f9f3271b8e68d14872
Isuru Fernando <isuruf@gmail.com>
2020-09-19 15:32:39 +0000

    win fix?

--- recipe/install-rust-std.bat
+++ recipe/install-rust-std.bat
@@ -1,3 +1,6 @@
+set MSYSTEM=MINGW%ARCH%
+set MSYS2_PATH_TYPE=inherit
+set CHERE_INVOKING=1
 FOR /F "delims=" %%i in ('cygpath.exe -u "%PREFIX%/Library"') DO set "pfx=%%i"
-bash %RECIPE_DIR%/install-rust-std.sh
+bash -lce "%RECIPE_DIR%/install-rust-std.sh"
 if errorlevel 1 exit 1

e84303b4f5e9afcb23f8d0408514b09b401c62e9
Isuru Fernando <isuruf@gmail.com>
2020-09-25 14:24:29 -0500

    fix SRC_DIR

--- recipe/install-rust.bat
+++ recipe/install-rust.bat
@@ -1,3 +1,8 @@
-FOR /F "delims=" %%i in ('cygpath.exe -u "%PREFIX%/Library"') DO set "pfx=%%i"
-bash %RECIPE_DIR%/install-rust.sh
+set MSYSTEM=MINGW%ARCH%
+set MSYS2_PATH_TYPE=inherit
+set CHERE_INVOKING=1
+FOR /F "delims=" %%i in ('cygpath.exe -u "%PREFIX%/Library"') DO set "PREFIX=%%i"
+FOR /F "delims=" %%i in ('cygpath.exe -u "%SRC_DIR%"') DO set "SRC_DIR=%%i"
+copy "%RECIPE_DIR%\install-rust.sh" .
+bash -lce "%SRC_DIR%/install-rust.sh"
 if errorlevel 1 exit 1

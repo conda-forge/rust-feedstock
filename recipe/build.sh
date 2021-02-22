638629117f1d67370b888cdc325656c167c55490
Marcel Bargull <marcel.bargull@udo.edu>
2021-02-22 13:26:15 +0100

    Replace prefix paths in install.log

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,3 +11,14 @@ DESTDIR=$PWD/destdir/
 # To do that, let's use destdir and then use the manifest-rust-std-* file
 # to install the files corresponding to rust-std
 ./install.sh --prefix=$PREFIX --destdir=$DESTDIR
+
+# install.log is large because it records full paths for each file.
+# => conda-build is slow to parse ripgrep's output for prefix replacement.
+# => replace path records beforehand:
+install_log="${DESTDIR}${PREFIX}/lib/rustlib/install.log"
+sed \
+  -e "s|${PREFIX}|/prefix|g" \
+  -e "s|${DESTDIR}|/destdir|g" \
+  -e "s|${PWD}|/source|g" \
+  -i.bak "${install_log}"
+rm "${install_log}.bak"

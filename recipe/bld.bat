2ea96a86e0ea5053958b0e5c149ac521bbf71820
Uwe L. Korn <uwe.korn@quantco.com>
2023-08-31 16:29:57 +0200

    More verbosity

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,5 @@
+@echo on
+
 set MSYSTEM=MINGW%ARCH%
 set MSYS2_PATH_TYPE=inherit
 set CHERE_INVOKING=1
@@ -6,6 +8,12 @@ FOR /F "delims=" %%i in ('cygpath.exe -u "%SRC_DIR%"') DO set "SRC_DIR=%%i"
 copy "%RECIPE_DIR%\build.sh" .
 bash -lce "%SRC_DIR%/build.sh"
 if errorlevel 1 exit 1
+
+dir %LIBRARY_PREFIX%\share
+dir %LIBRARY_PREFIX%\share\doc
+dir %LIBRARY_PREFIX%\share\doc\rust
+dir %LIBRARY_PREFIX%\share\doc\rust\html
+dir %LIBRARY_PREFIX%\share\doc\rust\html\sysroot
 
 @rem We need to delete the two directory as otherwise conda-build treats them as a sysroot
 @rem See https://github.com/conda/conda-build/blob/d3b717760f550abd58832c629d7f72aed46ae5ca/conda_build/post.py#L1376-L1381
